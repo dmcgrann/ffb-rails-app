884ae421c70f128a09ea83816a6858a2875c64ac
David McGrann <dpmcgrann@gmail.com>
2018-12-01 11:52:35 -0500

    users can signup, login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
     end
 
     def create
-      @user = User.find_by(name: params[:user_name])
+      @user = User.find_by(username: params[:user_username])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-        render :new
+        flash[:alert] = "Please signup."
+        redirect_to signup_path
       end
     end
 
