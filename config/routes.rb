12252e3d87e23ee266caeba1cade0e0459d2c2fd
David McGrann <dpmcgrann@gmail.com>
2018-12-01 09:25:16 -0500

    generated sessions controller; added root/signup/login routes; created welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root "welcome#home"
+  get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  
   resources :users
 end
