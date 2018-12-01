157a6f15f09545065dbb9df47b4bfe22d1d86c28
David McGrann <dpmcgrann@gmail.com>
2018-12-01 10:06:15 -0500

    user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:name, :password, :money_paid)
+      params.require(:user).permit(:username, :password, :money_paid)
     end
 end
