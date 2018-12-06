4e59a9ac8dc9d8cb3d4349936529683876a7eee9
David McGrann <dpmcgrann@gmail.com>
2018-12-06 15:14:25 -0500

    testing users, oauth and layouts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 
   def current_user
-    User.find_by(:id => session[:user_id]) || User.where(:provider => 'google_oauth2', :uid => session[:user_id])
+    User.find_by(:id => session[:user_id])
   end
 
 end
