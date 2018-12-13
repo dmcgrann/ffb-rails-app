6b04f1b7325c47d167cf829da202b451630307f1
David McGrann <dpmcgrann@gmail.com>
2018-12-13 18:32:45 -0500

    added validations to team model; added new team link to league show page

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,10 @@
 class Team < ApplicationRecord
   belongs_to :user
-  belongs_to :league 
+  belongs_to :league
+
+  validates :name, :presence => true, :allow_blank => false
+  validates :name, :uniqueness => true
+  validates :name, :length => {:maximum => 30, :message => "30 character max."}
+
+
 end
