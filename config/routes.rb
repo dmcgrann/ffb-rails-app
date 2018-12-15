74091297847784df7484c9f626e89332e8348d9d
David McGrann <dpmcgrann@gmail.com>
2018-12-15 14:37:28 -0500

    users can now view and edit their teams; removed team _form view to customize fields for new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   get 'auth/google_oauth2/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
 
-  resources :users
+  resources :users do
+    resources :teams, only: [:index, :show, :update]
+  end
   resources :leagues do
     resources :teams
   end
