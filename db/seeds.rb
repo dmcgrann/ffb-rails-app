3373f3adda40da3575ce74bd4261c88712104c6b
David McGrann <dpmcgrann@gmail.com>
2018-12-15 15:04:30 -0500

    seed data to test player functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+players = Player.create([{first_name: "Tom", last_name: "Brady", position: "QB", nfl_team: "NE"},
+  {first_name: "Aaron", last_name: "Rodgers", position: "QB", nfl_team: "GB"},
+  {first_name: "Saquon", last_name: "Barkley", position: "RB", nfl_team: "NYG"},
+  {first_name: "Antonio", last_name: "Brown", position: "WR", nfl_team: "PIT"},
+  {first_name: "Melvin", last_name: "Gordon", position: "RB", nfl_team: "LAC"}])
