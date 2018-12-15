cfb5b643e693a7f190cfb22196926d4c1ad7cd00
David McGrann <dpmcgrann@gmail.com>
2018-12-15 15:59:48 -0500

    working on adding players to teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,10 @@
 class Team < ApplicationRecord
   belongs_to :user
   belongs_to :league
+  has_many :team_players
+  has_many :players, through: :team_players
+
+  accepts_nested_attributes_for :players
 
   validates :name, :presence => true, :allow_blank => false
   validates :name, :uniqueness => true
