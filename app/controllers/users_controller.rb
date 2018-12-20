2a44dfbe288fcf9552ea51e0c121c04587af93ad
David McGrann <dpmcgrann@gmail.com>
2018-12-20 13:46:50 -0500

    added site admin checkbox for new users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:name, :password, :uid, :provider, :oauth_token)
+      params.require(:user).permit(:name, :password, :admin, :uid, :provider, :oauth_token)
     end
 end
