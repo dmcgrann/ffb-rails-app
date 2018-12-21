8dab45db792a96d73d4b55e4c0eb1355dad43b81
David McGrann <dpmcgrann@gmail.com>
2018-12-21 15:28:00 -0500

    saving before quitting for day

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -32,12 +32,12 @@ class TeamsController < ApplicationController
   end
 
   def edit
-    if params[:user_id]
+    if params[:user_id] && current_user
       user = User.find_by(id: params[:user_id])
       @team = user.teams.find_by(id: params[:id])
       redirect_to user_teams_path(user), alert: "Team not found" if @team.nil?
     else
-      redirect_to 'root_path'
+      redirect_to root_path
     end
   end
 
