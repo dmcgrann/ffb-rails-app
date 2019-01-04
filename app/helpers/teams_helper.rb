daf76064923ade24b05458bf92884b84400c1da7
David McGrann <dpmcgrann@gmail.com>
2019-01-04 09:22:57 -0500

    update user select field on team new to use exiting team helper method (user_id_field) if admin

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,7 +1,7 @@
 module TeamsHelper
 
   def user_id_field(team)
-    if team.user.nil?
+    if team.user.nil? || current_user.admin
       select_tag "team[user_id]", options_from_collection_for_select(User.all, :id, :name)
     else
       hidden_field_tag "team[user_id]", team.user_id
