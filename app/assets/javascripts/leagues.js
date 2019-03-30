04bed6b04b3b5827fc899a494ae35296d907e2ba
David McGrann <dpmcgrann@gmail.com>
2019-03-30 17:13:51 -0400

    update style for league#index

--- app/assets/javascripts/leagues.js
+++ app/assets/javascripts/leagues.js
@@ -9,13 +9,14 @@ function getLeagues() {
     let id = $(this).data("id");
     $.get("/leagues/" + id + ".json", function(data){
       let league = data;
-      let leagueDetails = "<p>" + "<strong>Draft Type:</strong>" + " " + league["draft"] + "</p><p>" + "<strong>Scoring Style:</strong>" + " " + league["scoring"] + "</p>";
+      let leagueDetails = "<p>" + "<strong>Draft Type:</strong>" + " " + league["draft"] + "</p><p>" + "<strong>Scoring Style:</strong>" + " " + league["scoring"] + "</p>" + "<h4>" + "Teams" + "</h4>";
       $("#league-" + id).html(leagueDetails);
 
       let teams = league["teams"];
       let teamList = "";
       teams.forEach(function(team) {
-        teamList += '<li class="js-team" data-id="' + team["id"] + '">' + team["team_name"] + '</li>';
+
+        teamList += '<p data-id="' + team["id"] + '">' + team["team_name"] + '</p>';
       });
       $("#league-" + id + "-teams").html(teamList);
     })
