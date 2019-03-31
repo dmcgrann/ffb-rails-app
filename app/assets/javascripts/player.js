8c1a35c84e33b932842a4e887f95bcc6d3040359
David McGrann <dpmcgrann@gmail.com>
2019-03-31 15:42:59 -0400

    add prototype to list players with name and team; add empty() to clear data on click.

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -15,22 +15,53 @@
 // }
 
 $(document).on('ready turbolinks:load', function() {
-  listQBs();
+  attachListeners();
 })
 
-function listQBs() {
-  $(".qbs").click(function(e){
-    e.preventDefault();
+function Player(player_name, position, nfl_team) {
+  this.player_name = name;
+  this.position = position;
+  this.nfl_team = team;
+}
 
+Player.prototype.entry = function() {
+  return `${this.player_name}, ${this.nfl_team}`
+}
+
+function listQBs() {
+  $("a.qbs").click(function(e){
+    e.preventDefault();
     $.get("/players" + ".json", function(data){
     let list = data;
     let quarterBacks = "";
       list.forEach(function(player){
         if (player["position"] === "QB") {
-          quarterBacks += "<p>" + player["player_name"] + "</p>"
+          quarterBacks += "<p>" + Player.prototype.entry.call(player) + "</p>"
         }
       })
-    $("#QBs").html(quarterBacks);
+    $("#players").html('').prepend(quarterBacks);
     })
   })
+}
+
+function listRBs() {
+  $("a.rbs").click(function(e){
+    e.preventDefault();
+    $.get("/players" + ".json", function(data){
+    let list = data;
+    let runningBacks = "";
+      list.forEach(function(player){
+        if (player["position"] === "RB") {
+          runningBacks += "<p>" + Player.prototype.entry.call(player) + "</p>"
+        }
+      })
+    $("#players").html('').prepend(runningBacks);
+    })
+  })
+}
+
+function attachListeners() {
+
+  listQBs();
+  listRBs();
 }
