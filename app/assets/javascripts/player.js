4842afd5edeef0a6e624809d518cc0ed6d92549b
David McGrann <dpmcgrann@gmail.com>
2019-05-03 10:37:25 -0400

    refactored player js

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -13,123 +13,37 @@ Player.prototype.listPlayers = function() {
 }
 
 
-function listQBs() {
-  $("a.qbs").click(function(e){
+function lists() {
+  $(".anchors").click(function(e){
     e.preventDefault();
+    let option = e.target.id;
     $.get("/players" + ".json", function(data){
       list = []
-      data.forEach(function(qb){
-        const playerAttrs = new Player(qb);
+      data.forEach(function(player){
+        const playerAttrs = new Player(player);
         const playerDetails = playerAttrs.listPlayers();
-        if (qb["position"] === "QB") {
+        if (player["position"] === "QB" && option == "qbs") {
           list.push(playerDetails)
-        };
-
-      });
-    $("#players").html('').prepend(list);
-  });
-    e.stopImmediatePropagation();
-  });
-}
-
-function listRBs() {
-  $("a.rbs").click(function(e){
-    e.preventDefault();
-    $.get("/players" + ".json", function(data){
-      list = []
-      data.forEach(function(rb){
-        const playerAttrs = new Player(rb);
-        const playerDetails = playerAttrs.listPlayers();
-        if (rb["position"] === "RB") {
-          list.push(playerDetails)
-        };
-
-      });
-    $("#players").html('').prepend(list);
-  });
-    e.stopImmediatePropagation();
-  });
-}
-
-function listWRs() {
-  $("a.wrs").click(function(e){
-    e.preventDefault();
-    $.get("/players" + ".json", function(data){
-      list = []
-      data.forEach(function(wr){
-        const playerAttrs = new Player(wr);
-        const playerDetails = playerAttrs.listPlayers();
-        if (wr["position"] === "WR") {
-          list.push(playerDetails)
-        };
-
-      });
-    $("#players").html('').prepend(list);
-  });
-    e.stopImmediatePropagation();
-  });
-}
-
-function listTEs() {
-  $("a.tes").click(function(e){
-    e.preventDefault();
-    $.get("/players" + ".json", function(data){
-      list = []
-      data.forEach(function(te){
-        const playerAttrs = new Player(te);
-        const playerDetails = playerAttrs.listPlayers();
-        if (te["position"] === "TE") {
-          list.push(playerDetails)
-        };
-
-      });
-    $("#players").html('').prepend(list);
-  });
-    e.stopImmediatePropagation();
-  });
-}
-
-function listByTeam() {
-  $("a.teams").click(function(e){
-    e.preventDefault();
-    $.get("/players" + ".json", function(data){
-      data.sort(function(a, b) {
-        const nameA = a.nfl_team.toUpperCase(); // ignore upper and lowercase
-        const nameB = b.nfl_team.toUpperCase(); // ignore upper and lowercase
-        if (nameA < nameB) {
-          return -1;
         }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-    });
 
-    list = []
-    data.forEach(function(sorted){
-      const playerAttrs = new Player(sorted);
-      const playerDetails = playerAttrs.listPlayers();
-        list.push(playerDetails)
-      });
-      $("#players").html('').prepend(list);
-    });
-    e.stopImmediatePropagation();
-  });
-}
-
-function allPlayers() {
-  $("a.all").click(function(e){
-    e.preventDefault();
-    $.get("/players" + ".json", function(data){
-      list = []
-      data.forEach(function(all){
-        const playerAttrs = new Player(all);
-        const playerDetails = playerAttrs.listPlayers();
+        if (player["position"] === "RB" && option == "rbs") {
           list.push(playerDetails)
+        }
+
+        if (player["position"] === "WR" && option == "wrs") {
+          list.push(playerDetails)
+        }
+
+        if (player["position"] === "TE" && option == "tes") {
+          list.push(playerDetails)
+        }else if (option == "all"){
+          list.push(playerDetails)
+        };
+
       });
     $("#players").html('').prepend(list);
   });
-  e.stopImmediatePropagation();
+    e.stopImmediatePropagation();
   });
 }
 
@@ -153,11 +67,6 @@ function nextPlayer() {
 }
 
 function playerListeners() {
-  listQBs();
-  listRBs();
-  listWRs();
-  listTEs();
-  allPlayers();
+  lists();
   nextPlayer();
-  listByTeam();
 }
