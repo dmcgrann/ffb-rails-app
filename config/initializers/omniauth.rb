7a9b2a9fc3de190931d549574a9e0044a77c3005
David McGrann <dpmcgrann@gmail.com>
2021-01-26 20:56:27 -0500

    omniauth fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.allowed_request_methods = [:post, :get]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['google_client_id'], ENV['google_secret'],
