66041df596fe61a22264543b65cea60020047eb7
David McGrann <dpmcgrann@gmail.com>
2021-01-26 20:58:54 -0500

    comment out

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
-OmniAuth.config.logger = Rails.logger
-OmniAuth.config.allowed_request_methods = [:post, :get]
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['google_client_id'], ENV['google_secret'],
-  {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
+#OmniAuth.config.logger = Rails.logger
+
+
+#Rails.application.config.middleware.use OmniAuth::Builder do
+  #provider :google_oauth2, ENV['google_client_id'], ENV['google_secret'],
+  #{client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+#end
